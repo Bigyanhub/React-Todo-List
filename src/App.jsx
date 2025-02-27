e9e4a1af2a1bcbfbc42c67c05da9d0ca7dc2d6a5
Bigyanhub <bigyanbasnet849@gmail.com>
2025-02-27 10:55:08 +0545

    Final Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import { useEffect, useState } from "react";
-import "./styles.css";
 import { NewTodoForm } from "./NewTodoForm";
+import "./styles.css";
 import { TodoList } from "./TodoList";
 
 export default function App() {
-  const [todos, setTodos] = useState(()=>{
-    const localValue = localStorage.getItem('ITEMS');
-    if (localValue==null) return[]
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
     return JSON.parse(localValue);
   });
 
   useEffect(() => {
-    localStorage.setItem('ITEMS', JSON.stringify(todos));
-  }, [todos])
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(title) {
     setTodos((currentTodos) => {
       return [
         ...currentTodos,
-        {
-          id: crypto.randomUUID(),
-          title: newItem,
-          completed: false,
-        },
+        { id: crypto.randomUUID(), title, completed: false },
       ];
     });
   }
@@ -41,15 +38,15 @@ export default function App() {
 
   function deleteTodo(id) {
     setTodos((currentTodos) => {
-      return currentTodos.filter((todo) => todo.id != id);
+      return currentTodos.filter((todo) => todo.id !== id);
     });
   }
 
   return (
     <>
       <NewTodoForm onSubmit={addTodo} />
-      <h1 className="header">ToDo List</h1>
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   );
 }
